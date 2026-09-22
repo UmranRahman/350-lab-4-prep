7568d1995bcb9cca561cb5cd737a469a48ccda1a
UmranRahman <umranrahman0@gmail.com>
2026-09-22 13:33:13 -0600

    list vs array done

--- src/expand.cpp
+++ src/expand.cpp
@@ -2,10 +2,13 @@
 #include <cstddef>  // size_t
 #include <cstdint>  // uint64_t, uint32_t
 
+#include <iostream>
+
 // Expands the binary representation of input by a factor of scale.
 // e.g., expand(0b1111ull, 3) == 0b001001001001
 uint64_t expand(uint64_t input, uint32_t scale) {
-    
+    assert(scale >= 1);
+
     return 0;  // replace this with your impl!
 }
 
