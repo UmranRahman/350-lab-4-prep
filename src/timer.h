07baaed820555651d75a72a0be01a82376f6bc98
UmranRahman <umranrahman0@gmail.com>
2026-09-22 12:13:16 -0600

    Timer done

--- src/timer.h
+++ src/timer.h
@@ -1,4 +1,43 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <chrono>
+#include <iostream>
+#include <cstdint>
+
+class Timer {
+public:
+    using Nanos =  std::chrono::nanoseconds;
+    using Micros = std::chrono::microseconds;
+    using Millis = std::chrono::milliseconds;
+    using Seconds = std::chrono::seconds;
+    using Minutes = std::chrono::minutes;
+    using Hours = std::chrono::hours;
+
+    Timer(){ //Default constructor
+        restart();
+    }
+    void restart(){ //resets the state of the Timer as if it has only started timing now
+        lastTime = std::chrono::steady_clock::now();
+    }
+
+    template <typename T>
+    uint64_t click(){
+        auto now = std::chrono::steady_clock::now();
+        auto diff = std::chrono::duration_cast<T>(now - lastTime).count();
+        lastTime = now
+        return diff;
+    }
+
+    template <typename T> uint64_t glance(){
+        auto now = std::chrono::steady_clock::now();
+        auto diff = std::chrono::duration_cast<T>(now - lastTime).count();
+        return diff;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> lastTime;
+};
+
+
 #endif  // TIMER_H
